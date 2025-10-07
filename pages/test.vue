f8c4406b1dd44feaa7e8eb82096a41e7bc4faf32
marten-mertan <mertangp@bk.ru>
2025-10-08 02:24:36 +0300

    update nuxt, fix gh-pages redirect issues

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div :class="[$style.TestPage, 'page']">
+    <div class="container">
+      TEST
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module>
+.TestPage {
+  //
+}
+</style>
